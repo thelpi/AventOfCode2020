37df397efcdede5e3711cadb7d96a045f9084c75
Lpi <thelpi.v3@gmail.com>
2020-12-15 22:31:01 +0100

    Day 15

--- AventOfCode/Program.cs
+++ AventOfCode/Program.cs
@@ -11,9 +11,39 @@ namespace AventOfCode
     {
         static void Main(string[] args)
         {
-            var result = DayFourteen(false, false);
-            Console.WriteLine($"Result part. 2 is {result}");
+            var result = DayFifteen(false, false);
+            Console.WriteLine($"Result is {result}");
             System.Threading.Thread.Sleep(int.MaxValue);
+        }
+
+        // 28
+        private static long DayFifteen(bool firstPart, bool sample)
+        {
+            var expectedQuotesCount = firstPart ? 2020 : 30000000;
+
+            var quotes = GetContent(15, v => Convert.ToInt32(v), sample: sample);
+            var initialQuotesCount = quotes.Count;
+
+            var latestQuoteByValue = quotes
+                .Take(quotes.Count - 1)
+                .ToDictionary(v => v, v => quotes.IndexOf(v));
+
+            for (int i = initialQuotesCount; i < expectedQuotesCount; i++)
+            {
+                var currentQuotedValue = quotes[i - 1];
+                var isCurrentValueAlreadyQuoted = latestQuoteByValue.ContainsKey(currentQuotedValue);
+                var currentQuotedLatestQuote = !isCurrentValueAlreadyQuoted
+                    ? 0
+                    : i - (latestQuoteByValue[currentQuotedValue] + 1);
+                quotes.Add(currentQuotedLatestQuote);
+                if (!isCurrentValueAlreadyQuoted)
+                {
+                    latestQuoteByValue.Add(currentQuotedValue, 0);
+                }
+                latestQuoteByValue[currentQuotedValue] = quotes.Count - 2;
+            }
+
+            return quotes.Last();
         }
 
         // 131
