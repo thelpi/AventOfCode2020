43862bc9b5372d0fbab0467af53dc1063c7bb8a9
Lpi <thelpi.v3@gmail.com>
2020-12-28 15:16:40 +0100

    Day 4 clean

--- AventOfCode/Day04.cs
+++ AventOfCode/Day04.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace AventOfCode
 {
@@ -10,176 +9,139 @@ namespace AventOfCode
     /// </summary>
     public sealed class Day04 : DayBase
     {
+        private const string BYR = "byr";
+        private const string IYR = "iyr";
+        private const string EYR = "eyr";
+        private const string HGT = "hgt";
+        private const string HCL = "hcl";
+        private const string ECL = "ecl";
+        private const string PID = "pid";
+        private readonly string[] CRITERIA_CODES = new string[]
+        {
+            BYR, IYR, EYR, HGT, HCL, ECL, PID
+        };
+
+        private const int BYR_MIN = 1920;
+        private const int BYR_MAX = 2002;
+
+        private const int IYR_MIN = 2010;
+        private const int IYR_MAX = 2020;
+
+        private const int EYR_MIN = 2020;
+        private const int EYR_MAX = 2030;
+
+        private const string HGT_IN_UNIT = "in";
+        private const string HGT_CM_UNIT = "cm";
+        private static readonly IReadOnlyDictionary<string, (int, int)> HGT_UNIT_RANGE =
+            new Dictionary<string, (int, int)>
+            {
+                { HGT_IN_UNIT, (59, 76) },
+                { HGT_CM_UNIT, (150, 193) }
+            };
+
+        private const int PID_LENGTH = 9;
+        private const string PID_ALLOWED_CARS = "0123456789";
+
+        private const int HCL_LENGTH = 7;
+        private const char HCL_FIRST_CAR = '#';
+        private const string HCL_ALLOWED_CARS = "0123456789abcdef";
+
+        private readonly string[] ALLOWED_ECL_COLORS = new string[]
+        {
+            "amb", "blu", "brn", "gry", "grn", "hzl", "oth"
+        };
+
         public Day04() : base(4) { }
 
         public override long GetFirstPartResult(bool sample)
         {
-
-            var codes = new string[]
-            {
-                "byr", "iyr", "eyr", "hgt",
-                "hcl", "ecl", "pid"
-            };
-
-            var passports = GetContent(v => v.Replace("\r\n", " "), "\r\n\r\n", sample: sample);
-
-            int passportsValid = 0;
-            foreach (var passport in passports)
-            {
-                var kpvList = new Dictionary<string, string>();
-                foreach (var p in passport.Split(' '))
-                {
-                    var split = p.Split(':');
-                    kpvList.Add(split[0].ToLowerInvariant(), split[1].ToLowerInvariant());
-                }
-                bool allCodesContained = codes.All(c => kpvList.ContainsKey(c));
-                
-                if (allCodesContained)
-                {
-                    passportsValid++;
-                }
-            }
-
-            return passportsValid;
+            return CountValidPassports(sample, valuesByCriteriaCode => true);
         }
 
         public override long GetSecondPartResult(bool sample)
         {
-
-            var codes = new string[]
+            return CountValidPassports(sample, valuesByCriteriaCode =>
             {
-                "byr", "iyr", "eyr", "hgt",
-                "hcl", "ecl", "pid"
-            };
+                bool isInvalidPassport = false;
+                foreach (var criteriaCode in CRITERIA_CODES)
+                {
+                    var codeValue = valuesByCriteriaCode[criteriaCode];
+                    switch (criteriaCode)
+                    {
+                        case BYR:
+                            isInvalidPassport = CheckInvalidRangedValue(codeValue, (BYR_MIN, BYR_MAX));
+                            break;
+                        case IYR:
+                            isInvalidPassport = CheckInvalidRangedValue(codeValue, (IYR_MIN, IYR_MAX));
+                            break;
+                        case EYR:
+                            isInvalidPassport = CheckInvalidRangedValue(codeValue, (EYR_MIN, EYR_MAX));
+                            break;
+                        case HGT:
+                            isInvalidPassport = (!codeValue.EndsWith(HGT_CM_UNIT) && !codeValue.EndsWith(HGT_IN_UNIT))
+                                || CheckInvalidRangedValue(codeValue.Substring(0, codeValue.Length - 2), HGT_UNIT_RANGE[codeValue.Substring(codeValue.Length - 2)]);
+                            break;
+                        case HCL:
+                            isInvalidPassport = codeValue[0] != HCL_FIRST_CAR
+                                || codeValue.Length != HCL_LENGTH
+                                || !codeValue.Substring(1, 6).All(c => HCL_ALLOWED_CARS.Contains(c));
+                            break;
+                        case ECL:
+                            isInvalidPassport = !ALLOWED_ECL_COLORS.Contains(codeValue);
+                            break;
+                        case PID:
+                            isInvalidPassport = codeValue.Length != PID_LENGTH
+                                || !codeValue.All(chardd => PID_ALLOWED_CARS.Contains(chardd));
+                            break;
+                    }
 
-            var passports = GetContent(v => v.Replace("\r\n", " "), "\r\n\r\n", sample: sample);
+                    if (isInvalidPassport)
+                    {
+                        break;
+                    }
+                }
 
-            int passportsValid = 0;
+                return !isInvalidPassport;
+            });
+        }
+
+        private long CountValidPassports(bool sample,
+            Func<Dictionary<string, string>, bool> validityCheck)
+        {
+            var passports = GetContent(v =>
+                v.Replace("\r\n", " "),
+                "\r\n\r\n",
+                sample: sample);
+
+            int countValidPassports = 0;
             foreach (var passport in passports)
             {
-                var kpvList = new Dictionary<string, string>();
-                foreach (var p in passport.Split(' '))
+                var valuesByCriteriaCode = new Dictionary<string, string>();
+                foreach (var passportRow in passport.Split(' '))
                 {
-                    var split = p.Split(':');
-                    kpvList.Add(split[0].ToLowerInvariant(), split[1].ToLowerInvariant());
+                    var passportRowParts = passportRow.Split(':');
+                    valuesByCriteriaCode.Add(
+                        passportRowParts[0].ToLowerInvariant(),
+                        passportRowParts[1].ToLowerInvariant());
                 }
-                bool allCodesContained = codes.All(c => kpvList.ContainsKey(c));
-                if (allCodesContained)
+
+                if (CRITERIA_CODES.All(c => valuesByCriteriaCode.ContainsKey(c)))
                 {
-                    bool invalid = false;
-                    foreach (var c in codes)
+                    if (validityCheck(valuesByCriteriaCode))
                     {
-                        var v = kpvList[c];
-                        switch (c)
-                        {
-                            case "byr":
-                                if (int.TryParse(v, out int byrReal))
-                                {
-                                    if (byrReal < 1920 || byrReal > 2002)
-                                    {
-                                        invalid = true;
-                                    }
-                                }
-                                else
-                                {
-                                    invalid = true;
-                                }
-                                break;
-                            case "iyr":
-                                if (int.TryParse(v, out int iyrReal))
-                                {
-                                    if (iyrReal < 2010 || iyrReal > 2020)
-                                    {
-                                        invalid = true;
-                                    }
-                                }
-                                else
-                                {
-                                    invalid = true;
-                                }
-                                break;
-                            case "eyr":
-                                if (int.TryParse(v, out int eyrReal))
-                                {
-                                    if (eyrReal < 2020 || eyrReal > 2030)
-                                    {
-                                        invalid = true;
-                                    }
-                                }
-                                else
-                                {
-                                    invalid = true;
-                                }
-                                break;
-                            case "hgt":
-                                if (v.EndsWith("cm"))
-                                {
-                                    v = v.Substring(0, v.Length - 2);
-                                    if (!int.TryParse(v, out int heightCm))
-                                    {
-                                        invalid = true;
-                                    }
-                                    else if (heightCm < 150 || heightCm > 193)
-                                    {
-                                        invalid = true;
-                                    }
-                                }
-                                else if (v.EndsWith("in"))
-                                {
-                                    v = v.Substring(0, v.Length - 2);
-                                    if (!int.TryParse(v, out int heightIn))
-                                    {
-                                        invalid = true;
-                                    }
-                                    else if (heightIn < 59 || heightIn > 76)
-                                    {
-                                        invalid = true;
-                                    }
-                                }
-                                else
-                                {
-                                    invalid = true;
-                                }
-                                break;
-                            case "hcl":
-                                if (!v.StartsWith("#") || v.Length != 7)
-                                {
-                                    invalid = true;
-                                }
-                                else
-                                {
-                                    v = v.Substring(1, 6);
-                                    var expected = "0123456789abcdef";
-                                    if (!v.All(charr => expected.Contains(charr)))
-                                    {
-                                        invalid = true;
-                                    }
-                                }
-                                break;
-                            case "ecl":
-                                string[] expectedd = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
-                                if (!expectedd.Contains(v))
-                                {
-                                    invalid = true;
-                                }
-                                break;
-                            case "pid":
-                                var expecteddd = "0123456789";
-                                if (v.Length != 9 || !v.All(chardd => expecteddd.Contains(chardd)))
-                                {
-                                    invalid = true;
-                                }
-                                break;
-                        }
-                    }
-                    if (!invalid)
-                    {
-                        passportsValid++;
+                        countValidPassports++;
                     }
                 }
             }
 
-            return passportsValid;
+            return countValidPassports;
+        }
+
+        private bool CheckInvalidRangedValue(string codeValue, (int, int) inclusiveMinMax)
+        {
+            return !int.TryParse(codeValue, out int intValue)
+                || intValue < inclusiveMinMax.Item1
+                || intValue > inclusiveMinMax.Item2;
         }
     }
 }
