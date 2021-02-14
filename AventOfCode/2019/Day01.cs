fe0bd5aa8ec7be6f6f4267ad46f391d052d2aeb6
Lpi <thelpi.v3@gmail.com>
2021-02-14 17:13:42 +0100

    First day 2019

---
+++ AventOfCode/2019/Day01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AventOfCode._2019
+{
+    /// <summary>
+    /// Day 1: The Tyranny of the Rocket Equation
+    /// </summary>
+    public sealed class Day01 : DayBase
+    {
+        public Day01() : base(2019, 1) { }
+
+        public override long GetFirstPartResult(bool sample)
+        {
+            return CommonTrunk(sample, 1);
+        }
+
+        public override long GetSecondPartResult(bool sample)
+        {
+            return CommonTrunk(sample, 2);
+        }
+
+        private long CommonTrunk(bool sample, int part)
+        {
+            var values = GetContent(
+                v => Convert.ToInt32(v),
+                sample: sample,
+                part: sample ? part : default(int?));
+
+            long massSum = 0;
+            int i = 0;
+            while (i < values.Count)
+            {
+                var value = values[i];
+                var addSum = Convert.ToInt32(Math.Floor(value / (decimal)3) - 2);
+                if (addSum > 0)
+                {
+                    massSum += addSum;
+                    if (part == 2)
+                    {
+                        values.Add(addSum);
+                    }
+                }
+                i++;
+            }
+
+            return massSum;
+        }
+    }
+}
---
+++ AventOfCodeUnitTests/Days2019Tests.cs
@@ -0,0 +1,24 @@
+﻿using AventOfCode._2019;
+using Xunit;
+
+namespace AventOfCodeUnitTests
+{
+    public class Days2019Tests
+    {
+        [Theory]
+        [InlineData(true, true, 34241)]
+        [InlineData(true, false, 3474920)]
+        [InlineData(false, true, 50346)]
+        [InlineData(false, false, 5209504)]
+        public void Day01Test(bool firstPart, bool sample, long expected)
+        {
+            var day = new Day01();
+
+            var result = firstPart
+                ? day.GetFirstPartResult(sample)
+                : day.GetSecondPartResult(sample);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
