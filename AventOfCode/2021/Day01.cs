20ff014b6dbd368fc2884a55bfe12bb28e2053b8
Lpi <thelpi.v3@gmail.com>
2022-01-08 23:58:03 +0100

    Day01 2021

---
+++ AventOfCode/2021/Day01.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AventOfCode._2021
+{
+    public sealed class Day01 : DayBase
+    {
+        public Day01() : base(2021, 1) { }
+
+        public override long GetFirstPartResult(bool sample)
+        {
+            var values = GetContent(v => Convert.ToInt32(v), sample: sample);
+
+            long sum = 0;
+            for (var i = 1; i < values.Count; i++)
+            {
+                if (values[i] > values[i - 1])
+                    sum++;
+            }
+
+            return sum;
+        }
+
+        public override long GetSecondPartResult(bool sample)
+        {
+            var values = GetContent(v => Convert.ToInt32(v), sample: sample);
+
+            long sum = 0;
+            for (var i = 3; i < values.Count; i++)
+            {
+                var oldV = values[i - 1] + values[i - 2] + values[i - 3];
+                var newV = values[i] + values[i - 1] + values[i - 2];
+
+                if (newV > oldV)
+                    sum++;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AventOfCodeUnitTests/Days2021Tests.cs
@@ -0,0 +1,24 @@
+﻿using AventOfCode._2021;
+using Xunit;
+
+namespace AventOfCodeUnitTests
+{
+    public class Days2021Tests
+    {
+        [Theory]
+        [InlineData(true, true, 7)]
+        [InlineData(true, false, 1233)]
+        [InlineData(false, true, 5)]
+        [InlineData(false, false, 1275)]
+        public void Day01Test(bool firstPart, bool sample, long expected)
+        {
+            var day = new Day01();
+
+            var result = firstPart
+                ? day.GetFirstPartResult(sample)
+                : day.GetSecondPartResult(sample);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
